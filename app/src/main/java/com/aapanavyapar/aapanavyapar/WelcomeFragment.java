52ae61b880ac8332244953da0575b4ac264e99be
root <root@kali.urp>
2021-02-11 11:31:51 +0530

    Splash Updated...git add /root/AndroidStudioProjects/aapanavyapar-android-user/app/src/main/java/com/aapanavyapar/aapanavyapar/WelcomeFragment.java /root/AndroidStudioProjects/aapanavyapar-android-user/app/src/main/res/layout/fragment_welcome.xml!

--- app/src/main/java/com/aapanavyapar/aapanavyapar/WelcomeFragment.java
+++ app/src/main/java/com/aapanavyapar/aapanavyapar/WelcomeFragment.java
@@ -1,9 +1,17 @@
 package com.aapanavyapar.aapanavyapar;
-
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,6 +23,9 @@ import androidx.navigation.fragment.NavHostFragment;
 import java.util.concurrent.TimeUnit;
 
 public class WelcomeFragment extends Fragment {
+    Animation txt_anim,bounce;
+    ImageView logo;
+    TextView title;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,8 +36,22 @@ public class WelcomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
         //After Completion of Animation
+
+        //Animation   ***
+        //top = AnimationUtils.loadAnimation(this,R.anim.top_anim);
+        txt_anim = AnimationUtils.loadAnimation(this,R.anim.txt_anim);
+        bounce = AnimationUtils.loadAnimation(this,R.anim.bounce);
+
+
+        logo = findViewById(R.id.imageView);
+        title = findViewById(R.id.textView2);
+
+        //Applying Animation   ***
+        //logo.setAnimation(top);
+        title.setAnimation(txt_anim);
+        logo.setAnimation(bounce);
+
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
