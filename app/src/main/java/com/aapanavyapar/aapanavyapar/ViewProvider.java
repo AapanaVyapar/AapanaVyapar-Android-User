2176d036d72a50efe83bfd2e51f5856709e390f2
Bhagyashri_Badgujar <bhagya.sv.badgujar@gmail.com>
2021-04-27 18:59:37 +0530

    Try to add toolbar

--- app/src/main/java/com/aapanavyapar/aapanavyapar/ViewProvider.java
+++ app/src/main/java/com/aapanavyapar/aapanavyapar/ViewProvider.java
@@ -1,15 +1,19 @@
 package com.aapanavyapar.aapanavyapar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.aapanavyapar.ProductAdapter;
 import com.aapanavyapar.ProductData;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipDrawable;
 import com.google.android.material.chip.ChipGroup;
@@ -20,11 +24,43 @@ public class ViewProvider extends AppCompatActivity {
 //    Chip chip;
 //    RecyclerView recycler_View;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_provider);
         Toast.makeText(this.getApplicationContext(), "View Provider...", Toast.LENGTH_LONG).show();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+              @Override
+              public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                  Fragment selectedFragment = null;
+                  switch (item.getItemId()) {
+                      case R.id.av_trend:
+                          selectedFragment = new TrendingFragment();
+                          break;
+                      case R.id.av_cart:
+                          selectedFragment = new CartFragment();
+                          break;
+                      case R.id.av_search:
+                          selectedFragment = new SearchFragment();
+                          break;
+                      case R.id.av_orders:
+                          selectedFragment = new OrderFragment();
+                          break;
+                      case R.id.av_profile:
+                          selectedFragment = new ProfileFragment();
+                          break;
+                  }
+                  getSupportFragmentManager().beginTransaction().replace(R.id.linear_layout1, selectedFragment).commit();
+                  return true;
+              }
+        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.linear_layout1,new TrendingFragment()).commit();
+
+
 
 //        String arr[] = {"Food", "Clothes", "Electronics", "Devotional", "Sports", "Cosmetics"};
 //
@@ -61,5 +97,8 @@ public class ViewProvider extends AppCompatActivity {
 //            ProductAdapter product_adapter = new ProductAdapter(product_data,ViewProvider.this);
 //            recycler_View.setAdapter(product_adapter);
 //        }
+
     }
+
+
 }
