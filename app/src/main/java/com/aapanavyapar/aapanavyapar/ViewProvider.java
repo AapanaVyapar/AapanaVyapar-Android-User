b8ca34b94ed6b93e3c6f4dcc557b395bf6f2904c
shitij <21.shitijagrawal@gmail.com>
2021-04-27 19:27:35 +0530

    bottom app bar added

--- app/src/main/java/com/aapanavyapar/aapanavyapar/ViewProvider.java
+++ app/src/main/java/com/aapanavyapar/aapanavyapar/ViewProvider.java
@@ -20,11 +20,6 @@ import com.google.android.material.chip.ChipGroup;
 
 public class ViewProvider extends AppCompatActivity {
 
-//    ChipGroup chipGroup;
-//    Chip chip;
-//    RecyclerView recycler_View;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,72 +28,30 @@ public class ViewProvider extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-              @Override
-              public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                  Fragment selectedFragment = null;
-                  switch (item.getItemId()) {
-                      case R.id.av_trend:
-                          selectedFragment = new TrendingFragment();
-                          break;
-                      case R.id.av_cart:
-                          selectedFragment = new CartFragment();
-                          break;
-                      case R.id.av_search:
-                          selectedFragment = new SearchFragment();
-                          break;
-                      case R.id.av_orders:
-                          selectedFragment = new OrderFragment();
-                          break;
-                      case R.id.av_profile:
-                          selectedFragment = new ProfileFragment();
-                          break;
-                  }
-                  getSupportFragmentManager().beginTransaction().replace(R.id.linear_layout1, selectedFragment).commit();
-                  return true;
-              }
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()){
+                    case R.id.av_trend:
+                        selectedFragment = new TrendingFragment();
+                        break;
+                    case R.id.av_cart:
+                        selectedFragment = new CartFragment();
+                        break;
+                    case R.id.av_search:
+                        selectedFragment = new SearchFragment();
+                        break;
+                    case R.id.av_orders:
+                        selectedFragment = new OrderFragment();
+                        break;
+                    case R.id.av_profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                return true;
+            }
         });
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.linear_layout1,new TrendingFragment()).commit();
-
-
-
-//        String arr[] = {"Food", "Clothes", "Electronics", "Devotional", "Sports", "Cosmetics"};
-//
-//
-//        chipGroup = findViewById(R.id.chipgroup);
-//        for (int i = 0; i < arr.length; i++) {
-//            chip = new Chip(this);
-//            chip.setText(arr[i]);
-//            ChipDrawable chipDrawable = ChipDrawable.createFromAttributes(this, null, 0, R.style.CustomChipStyle);
-//            chip.setChipDrawable(chipDrawable);
-//            chip.setId(i);
-//            chip.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-////                  Chip chipClick = (Chip) v;
-//                    Chip chipClick = v.findViewById(v.getId());
-//                    Toast.makeText(getApplicationContext(), chipClick.getText(), Toast.LENGTH_LONG).show();
-//                }
-//            });
-//
-//            chipGroup.addView(chip);
-//
-//            recycler_View = findViewById(R.id.recycler_view);
-//            recycler_View.setHasFixedSize(true);
-//            recycler_View.setLayoutManager(new LinearLayoutManager(this));
-//
-//            ProductData[] product_data = new ProductData[]{
-//                    // new product_data(R.drawable.watch,"Rolex Women's Watch","Fashion Wrist Watches","Shop Description"),
-////                    new product_data(R.drawable.laptop,"Acer Laptop","Sonam Electronics","Shop Description"),
-////                    new product_data(R.drawable.burger,"Classic Cheeseburger","We Desi","Shop Description"),
-////                    new product_data(R.drawable.third_image,"Yonex Badminton Kit","Jalgaon Sports","Shop Description"),
-//            };
-//
-//            ProductAdapter product_adapter = new ProductAdapter(product_data,ViewProvider.this);
-//            recycler_View.setAdapter(product_adapter);
-//        }
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new TrendingFragment()).commit();
     }
-
-
 }
