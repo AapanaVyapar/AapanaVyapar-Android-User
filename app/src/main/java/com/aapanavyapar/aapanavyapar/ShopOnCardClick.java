69b4f40f969fe6f6115d386cbae588219b22c2b2
Bhagyashri_Badgujar <bhagya.sv.badgujar@gmail.com>
2021-06-02 15:10:56 +0530

    Added comment section and added offer image. With the help of snehal

--- app/src/main/java/com/aapanavyapar/aapanavyapar/ShopOnCardClick.java
+++ app/src/main/java/com/aapanavyapar/aapanavyapar/ShopOnCardClick.java
@@ -2,15 +2,27 @@ package com.aapanavyapar.aapanavyapar;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.aapanavyapar.adapter.ProductAdapter;
+import com.aapanavyapar.viewData.ProductData;
+
+import java.util.ArrayList;
+
 
 public class ShopOnCardClick extends Fragment {
 
+
+    ProductAdapter productAdapter;
+    RecyclerView recyclerView ;
 
     public ShopOnCardClick() {
 
@@ -21,6 +33,18 @@ public class ShopOnCardClick extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        ArrayList<ProductData> productData = new ArrayList<>();
+        productAdapter = new ProductAdapter(productData, getContext());
+        recyclerView.setAdapter(productAdapter);
+
 
     }
 
